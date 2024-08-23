8c245ea41f3765b733ccc1aabc49636907956e5e
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-08-23 07:04:17 +0200

    Update config.py

--- predictions/config.py
+++ predictions/config.py
@@ -1,3 +1,12 @@
+"""
+This script contains the hyperparameters for a time series prediction model using LSTM (Long Short-Term Memory) networks.
+The model is trained on a portion of the dataset (specified by TRAIN_SPLIT), and the remaining portion is used for testing.
+The model uses a look-back mechanism (LOOK_BACK) to consider a certain number of previous time steps when making predictions.
+The model's performance is compared for different look-ahead values (LOOK_AHEAD_VALUES), which are the number of time steps into the future that the model predicts.
+The model is trained for a certain number of iterations (NB_EPOCHS), and the weights are updated in batches of a certain size (BATCH_SIZE).
+The LSTM network consists of three layers, with the number of neurons in each layer specified by NEURONS_1, NEURONS_2, and NEURONS_3.
+Dropout regularization is used to prevent overfitting, with the dropout rate specified by DROPOUT_RATE.
+"""
 # %% Hyper Parameters
 TRAIN_SPLIT = 0.8  # Percentage of train set from the whole dataset
 LOOK_BACK = 10  # Number of timesteps fed to the network to predict future timesteps
